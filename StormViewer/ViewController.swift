ffc561bf6817343b7658b97987adeeaac403a246
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-06 15:05:52 +0200

    Read images and display filename in console

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
 
 
